90db5f72d89d4ff9a808eafd4aa7ecdba8e7def5
Jonathan Schoonbroodt <33702771+jonathanschoonbroodt@users.noreply.github.com>
2023-11-18 12:06:52 +0100

    ui tweaking

--- js/captureBuilding.mjs
+++ js/captureBuilding.mjs
@@ -12,40 +12,36 @@ export function captureBuilding () {
       logToConsoleContainer('Unit hasn\'t the capture capacity.required')
       return
     }
-
     if (Number(globalVariables.buildingDatas.buildingCapturePoint) === 20 && Number(globalVariables.buildingDatas.buildingPlayerAppartenance) === getUnitData(globalVariables.selectedUnit).unitPlayer) {
       logToConsoleContainer('You already own this building.')
       return
     }
-
     logToConsoleContainer('This building can be captured by your unit. <span class="_color -green">press <span class="_text -bold">Spacebar</span> to capture</span> the building.')
-
     logToUIFeedbackContainer('<span class="_color -green">Press <span class="_text -bold">Spacebar</span> to capture</span> the building.')
-
-    document.addEventListener('keypress', startCapture)
-
-    function startCapture (event) {
-      if (event.code === 'Space' && globalVariables.selectedUnit.dataset.capture_capacity > 0) {
-        event.preventDefault()
-        preventCancelMove(globalVariables.selectedUnit)
-        const updatedCapturePoints = Number(globalVariables.buildingDatas.buildingCapturePoint) - 10
-        globalVariables.buildingDatas.building.setAttribute('data-capture_points', updatedCapturePoints)
-        if (updatedCapturePoints === 10) {
-          sounds.jumpCapture.play()
-          globalVariables.buildingDatas.building.classList.add('-halfcaptured')
-        }
-        if (updatedCapturePoints === 0) {
-          sounds.trumpetFanfare.volume = 0.5
-          sounds.trumpetFanfare.play()
-          globalVariables.buildingDatas.building.classList.remove('-capturedby1', '-capturedby2', '-halfcaptured')
-          globalVariables.buildingDatas.building.classList.add('-capturedby' + globalVariables.currentPlayer)
-          globalVariables.buildingDatas.building.setAttribute('data-player', globalVariables.currentPlayer)
-          globalVariables.buildingDatas.building.setAttribute('data-capture_points', 20)
-        }
-        globalVariables.selectedUnit.setAttribute('data-capture_capacity', 0)
-      }
-
-      document.removeEventListener('keypress', startCapture)
-    }
+    document.addEventListener('keypress', startCaptureBuilding)
   }
+}
+
+function startCaptureBuilding (event) {
+  if (event.code === 'Space' && globalVariables.selectedUnit.dataset.capture_capacity > 0) {
+    event.preventDefault()
+    preventCancelMove(globalVariables.selectedUnit)
+    const updatedCapturePoints = Number(globalVariables.buildingDatas.buildingCapturePoint) - 10
+    globalVariables.buildingDatas.building.setAttribute('data-capture_points', updatedCapturePoints)
+    if (updatedCapturePoints === 10) {
+      sounds.jumpCapture.play()
+      globalVariables.buildingDatas.building.classList.add('-halfcaptured')
+    }
+    if (updatedCapturePoints === 0) {
+      sounds.trumpetFanfare.volume = 0.5
+      sounds.trumpetFanfare.play()
+      globalVariables.buildingDatas.building.classList.remove('-capturedby1', '-capturedby2', '-halfcaptured')
+      globalVariables.buildingDatas.building.classList.add('-capturedby' + globalVariables.currentPlayer)
+      globalVariables.buildingDatas.building.setAttribute('data-player', globalVariables.currentPlayer)
+      globalVariables.buildingDatas.building.setAttribute('data-capture_points', 20)
+    }
+    globalVariables.selectedUnit.setAttribute('data-capture_capacity', 0)
+  }
+
+  document.removeEventListener('keypress', startCaptureBuilding)
 }
